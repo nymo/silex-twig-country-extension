efda144329be36eccbf650e112ac5709414efb04
Gregor Panek <gregor.panek@dominolaser.com>
2013-03-20 14:17:51 +0100

    -added country extension -added composer and readme file -excluded vendor and idea files from git

---
+++ nymo/README.md
@@ -0,0 +1,4 @@
+## Country extension for twig, to get full country name for country code
+
+### Usage
+todo
---
+++ nymo/Twig/Extension/CountryExtension.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * User: nymo
+ * Date: 20.03.13
+ * Time: 10:08
+ */
+namespace nymo\Twig\Extension;
+use Symfony\Component\Locale\Locale;
+class CountryExtension extends \Twig_Extension{
+
+    /**
+     * @var \Silex\Application
+     */
+    protected $app;
+
+    public function __construct(\Silex\Application $app){
+        $this->app = $app;
+    }
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return 'nymo_country';
+    }
+
+    /**
+     * @return array
+     */
+    public function getFunctions(){
+        return array('country' => new \Twig_Function_Method($this,'country'));
+    }
+
+    /**
+     * @param $key
+     * @return mixed
+     */
+    public function country($key){
+        $countries = Locale::getDisplayCountries($this->app['locale']);
+        return $countries[$key];
+    }
+
+}
